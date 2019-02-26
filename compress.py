19fbfd3d88e0af264a3306d750cc366d9db26f05
feiandxs <feiiiiiii@sc2l.com>
2019-02-26 17:44:06 +0800

    aaaaa

--- compress.py
+++ compress.py
@@ -5,12 +5,24 @@ from PIL import Image
 import os
 import sys
 import glob
+import shutil
 
 #设定目标输出路径文件夹
-desdir = "/lsh_des/"
-#输入文件夹路径
+#desdir = "/lsh_des/"
+#读取参数，输入文件夹路径
 url = sys.argv[1]
+#print(url)
+#目标文件夹上级父目录
+hjx = os.path.abspath(os.path.join(os.path.dirname(url),os.path.pardir))
+#源目录最后一段
+hw = os.path.basename(os.path.normpath(url))
+#新目录
+desdir = hjx + "/lsh_" + hw
 
+#唔，先，复制个文件夹出来再说 2333
+shutil.copytree(url,desdir)
+
+#先不管了，复制一个原始的文件目录出来， 万一还要用呢，对吧
 
 #创建文件夹
 def mkdir(path):
@@ -39,10 +51,11 @@ def dirlist(path, allfile):
             allfile.append(filepath)  
     return allfile 
 
-#list = dirlist("/Users/feiandxs/Dropbox/python_compress_pic/img/",[])
-list = dirlist(url,[])
-
+#列出目标文件夹下所有文件
+list = dirlist(desdir,[])
 print(list)
+
+
 
 # 默认压缩jpeg
 def default_compress_jpeg(path,extname):
@@ -65,8 +78,8 @@ for x in list:
     if extname in image_types:
 
 #在每个目录下创建对应的 des 目录用于存放压缩后的新图片
-        mkdir(path)
-        newpath = path + desdir + filename
+        #mkdir(path)
+        newpath = desdir + filename
         if extname == ".png":
             default_compress_png(fullpath,newpath)
             print(fullpath + "压缩成功")
