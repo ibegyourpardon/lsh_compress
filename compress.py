4dce6abe9fcbcd1899620d1546963e40cc084d1b
feiandxs <feiiiiiii@sc2l.com>
2019-02-27 11:42:04 +0800

    又优化了一些有的没的的东西

--- compress.py
+++ compress.py
@@ -12,23 +12,23 @@ from colorama import Fore, Back, Style
 start = time.clock()
 #读取参数
 #第一个参数为输入文件夹路径
-url = sys.argv[1]
+source_dir = sys.argv[1]
 #第二个参数为压缩质量
 thequality = int(sys.argv[2])
 
 print (thequality)
 print ('参数列表:', str(sys.argv))
 
-#print(url)
+#print(source_dir)
 #目标文件夹上级父目录
-hjx = os.path.abspath(os.path.join(os.path.dirname(url),os.path.pardir))
+hjx = os.path.abspath(os.path.join(os.path.dirname(source_dir),os.path.pardir))
 #源目录最后一段
-hw = os.path.basename(os.path.normpath(url))
+hw = os.path.basename(os.path.normpath(source_dir))
 #新目录
 desdir = hjx + "/lsh_" + hw
 
 #唔，先，复制个文件夹出来再说 2333
-shutil.copytree(url,desdir)
+shutil.copytree(source_dir,desdir)
 
 #读取文件列表
 def dirlist(path, allfile):  
@@ -44,6 +44,16 @@ def dirlist(path, allfile):
         else:
             allfile.append(filepath)  
     return allfile 
+
+
+#计算压缩前的文件夹大小
+folder_size = 0
+for (path, dirs, files) in os.walk(source_dir):
+    for file in files:
+        filename = os.path.join(path, file)
+        folder_size += os.path.getsize(filename)
+
+pre_size = "%0.1f MB" % (folder_size/(1024*1024.0))
 
 
 
@@ -76,13 +86,13 @@ for x in list:
         newpath = fullpath
         if extname == ".png":
             default_compress_png(fullpath,newpath)
-            print("正在压缩" + fullpath)
+            print(Fore.GREEN + "正在压缩  " + fullpath)
         elif extname == ".jpg":
             default_compress_jpeg(fullpath,newpath)
-            print("正在压缩" + fullpath)
+            print(Fore.GREEN + "正在压缩  " + fullpath)
         elif extname ==".jpeg":
             default_compress_jpeg(fullpath,newpath)
-            print("正在压缩" + fullpath)
+            print(Fore.GREEN + "正在压缩  " + fullpath)
         else:
             print(filename + "不是受支持的图片文件类型")
 
@@ -92,7 +102,18 @@ for x in list:
     else:
         print("incorrect type")
 
+# 计算压缩后的文件夹大小
+folder_size = 0
+for (path, dirs, files) in os.walk(desdir):
+    for file in files:
+        filename = os.path.join(path, file)
+        folder_size += os.path.getsize(filename)
+
+post_size = "%0.1f MB" % (folder_size / (1024 * 1024.0))
+
+
 #计算程序运行时间
 elapsed = (time.clock() - start)
 print(Fore.BLUE + "所有图片压缩完成,一共压缩了" + str(i) + "张图片")
-print (Fore.BLUE +  "图片压缩完成,耗时:" + str(elapsed) + "秒")
+print (Fore.RED +  "图片压缩完成,耗时:" + str(elapsed) + "秒")
+print("所有图片已从" + pre_size + "压缩到了" + post_size)
